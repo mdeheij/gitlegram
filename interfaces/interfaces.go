f4f0b1a7fe9506a247cc48b3e50e7fa72e5360c6
Mike de Heij <mike@deheij.email>
2017-09-07 01:59:44 +0200

    Add interface for user

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -2,8 +2,14 @@ package interfaces
 
 type RequestInterface interface {
 	GetRepository() RepositoryInterface
+	GetUser() (UserInterface, error)
 }
 
 type RepositoryInterface interface {
 	GetName() string
+}
+type UserInterface interface {
+	GetUsername() string
+	GetName() string
+	GetAvatarURL() string
 }
