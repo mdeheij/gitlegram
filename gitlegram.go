f1140169471aca5dff624bd3d83682b347864d8c
Mike de Heij <mike@deheij.email>
2017-09-07 02:02:43 +0200

    WIP: Add push message

--- gitlegram.go
+++ gitlegram.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/mdeheij/gitlegram/interfaces"
+import (
+	"github.com/mdeheij/gitlegram/interfaces"
+	log "github.com/mdeheij/logwrap"
+)
 
-func getMessage(r interfaces.RequestInterface) string {
-	announceMsg := "⬆️ " + r.GetRepository().GetName()
+func getMessage(r interfaces.Request) string {
+	user, err := r.GetUser()
+
+	if err != nil {
+		log.Critical("Could not fetch user: ", err)
+	}
+
+	announceMsg := "⬆️ " + r.GetRepository().GetName() + " by " + user.GetName()
 	return announceMsg
 }
